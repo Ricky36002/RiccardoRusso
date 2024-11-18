e831055e6a1e4f7ebd1dae9ae781c4217084c3e0
Ricky36002 <rrcyber8@gmail.com>
2024-11-18 14:52:15 +0100

    update

--- Rest2/CercaStringa/cercastringa.py
+++ Rest2/CercaStringa/cercastringa.py
@@ -1,62 +1,103 @@
 import os
 import mmap
 import PyPDF2
+import textract
+import googlegemini
+import subprocess
+
 
 sRoot = input("Inserisci la root directory: ")
 sStringaDaCercare = input("Inserisci la stringa da cercare: ")
-sOutDir = input("Inserisci la dir di output: ")
+SOutDir = input("Inserici la directory di output: ")
+iNumFileTrovati = 0
+res = []
 
 
-def CercaStringaInFileName(sFilename,sStringToSearch):
-    sFilename1 = sFilename.lower()
+
+def CercaStringaInFileName(sFile, sStringToSearch):
+    sFilename1 = sFile.lower()
     sStringToSearch1 = sStringToSearch.lower()
-    print("Cerco {1} in {0} ".format(sFilename1,sStringToSearch1))
+    print("Cerco {0} in {1}".format(sStringToSearch1, sFilename1))
     iRet = sFilename1.find(sStringToSearch1)
-    if(iRet>-1):
-        print("Trovato")
+    if(iRet>=0):
+        print("Trovato!")
         return True
     return False
 
-def CercaInFilePdf(sFile,sString):
-    object = PyPDF2.PdfFileReader(sFile)
-    numPages = object.getNumPages()
-    for i in range(0, numPages):
-        pageObj = object.getPage(i)
-        text = pageObj.extractText()
-        text = text.lower()
-        if(text.find(sString)!=-1):
-            return True
-    return False
-
-def CercaStringaInFileContent(sFile,sString):
+def CercaStringaInFileContent(sFile, sString):
     sString = sString.lower()
-    iLen = len(sString)
-    sOutFileName,sOutFileExt = os.path.splitext(sFile)
-    if(sOutFileExt.lower()==".pdf"):
-        print("Riconosciuto file pdf " + sFile)
-        return CercaInFilePdf(sFile,sString)
     try:
         with open(sFile) as f:
-            s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+            s = mmap.mmap(f.fileno(), 0, access = mmap.ACCESS_READ)
             sAppo = s.readline()
-            while len(sAppo)> 0 :
-                sAppo = sAppo.lower()
-                if(sAppo.find(sString.encode())!=-1):
+            while len(sAppo) > 0:
+                SAppo = SAppo.lower()
+                if sAppo.find(sString.encode() != -1):
                     return True
                 else:
                     sAppo = s.readline()
     except:
         return False
-    
-iNumFileTrovati = 0
+
+def CercaInFilePdf(sFile,sString):
+    object = PyPDF2.PdfReader(sFile)
+    numPages = len(object.pages)
+    for i in range(0, numPages):
+        pageObj = object.pages[i]
+        text = pageObj.extract_text()
+        text = text.lower()
+        if(text.find(sString)!=-1):
+           return True
+    return False
+
+def CercaInFileDoc(sFile,sString):
+    text = textract.process(sFile)
+    text = text.lower()
+    if(text.find(sString.encode())!=-1):
+        return True
+    return False
+
+
 for root, dirs, files in os.walk(sRoot):
-    sToPrint = "Dir corrente {0} contenente {1} subdir e {2} files".format(root, len(dirs), len(files))
+    sToPrint = "Dir corrente {0} contenent {1} subdir e {2} files".format(root,len(dirs),len(files))
     print(sToPrint)
+
     for filename in files:
-        iRet = CercaStringaInFileName(filename,sStringaDaCercare)
-        if(iRet != True):
-            sFilePathCompleto = os.path.join(root, filename)
-            iRet = CercaStringaInFileContent(sFilePathCompleto, sStringaDaCercare)
-        if(iRet == True):
-            print("Trovato file: ",filename) 
-        iNumFileTrovati = iNumFileTrovati + 1
+        
+        iRet = CercaStringaInFileName(filename, sStringaDaCercare)
+        if iRet == True:
+            print("Trovato file: ", filename)
+            iNumFileTrovati += 1
+            res.append(pathCompleto)
+        else:
+            pathCompleto = os.path.join(root,filename)
+            iRet = CercaStringaInFileName(pathCompleto, sStringaDaCercare)
+        
+            sOutFileName,sOutFileExt = os.path.splitext(filename)
+
+            if sOutFileExt.lower()==".pdf":
+                iRet = CercaInFilePdf(pathCompleto,sStringaDaCercare)
+                if iRet == True:
+                    print("Trovato file: ", filename)
+                    iNumFileTrovati += 1
+                    res.append(pathCompleto)
+
+            elif sOutFileExt.lower()==".doc":
+                print(CercaInFileDoc(pathCompleto,sStringaDaCercare))
+                if iRet == True:
+                    print("Trovato file: ", filename)
+                    iNumFileTrovati += 1
+                    res.append(pathCompleto)
+
+            elif sOutFileExt.lower() in [".jpg", ".gif"]:
+                iRet = googlegemini.CercaIMGGemini(pathCompleto,sStringaDaCercare)
+                subprocess.run(["rm", "./temp_img/image.jpg"])
+                subprocess.run(["rm", "./request.json"])
+                if iRet == True:
+                    print("Trovato file: ", filename)
+                    iNumFileTrovati += 1
+                    res.append(pathCompleto)
+            else:
+                print(CercaStringaInFileContent(pathCompleto,sStringaDaCercare))
+
+print(iNumFileTrovati, res)
